60de70ba478e5c5dcf802b9d67e22eed4b20abd9
Ryan <ryananthonyrichardson@gmail.com>
2024-12-07 13:31:14 -0800

    test: prove a game can start and update segment display

--- tests/test_general.py
+++ tests/test_general.py
@@ -16,21 +16,32 @@ class TestGeneral(MpfMachineTestCase):
         ))
 
     def test_game_start(self):
-        self.__machine_boots
-        self.__game_begins
+        self._machine_boots
+        self._game_begins
+        self._ball_live
 
-    def __machine_boots(self):
+    def _machine_boots(self):
         self.assertEqual(3,
             self.machine.ball_devices.bd_trough.balls)
         self.assertModeRunning('attract')
         self.assertModeNotRunning('base')
 
-    def __game_begins(self):
+    def _game_begins(self):
         self.hit_and_release_switch("s_start")
         self.advance_time_and_run(1)
+        self.assertModeRunning("base")
+        self.assertModeNotRunning("attract")
         self.assertEqual(2,
-            self.machine.ball_devices.bd_trough.balls)
+            self.machine.ball_devices["bd_trough"].balls)
         self.assertEqual(1,
-            self.machine.ball_devices.bd_shooter_lane.balls)
-        self.assertModeNotRunning('attract')
-        self.assertModeRunning('base')
+            self.machine.ball_devices["bd_shooter_lane"].balls)
+
+    def _ball_live(self):
+        self.hit_and_release_switch("s_shooter_lane")
+        self.hit_and_release_switch("s_grand_advance")
+        self.advance_time_and_run(4)
+        self.assertEqual(1, self.machine.playfield.balls)
+        self.assertEqual(0,
+            self.machine.ball_devices["bd_shooter_lane"].balls)
+        self.hit_and_release_switch("s_start")
+        self.advance_time_and_run(1)
