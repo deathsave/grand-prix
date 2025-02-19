a7c3eeee2d54b57fcecded3334ab857aa6b8efe9
Ryan <ryananthonyrichardson@gmail.com>
2025-02-19 01:42:11 -0800

    feat: have some fun with the segments in attract

---
+++ modes/attract/code/attract.py
@@ -0,0 +1,40 @@
+import random
+from mpf.core.mode import Mode
+from mpf.modes.attract.code.attract import Attract
+
+class GrandPrixAttract(Attract):
+
+    def mode_start(self, **kwargs):
+        self.machine.timers["segment_wiggle_timer"].reset()
+        timer_event = "timer_segment_wiggle_timer_tick"
+        self.add_mode_event_handler(timer_event,
+            self.wiggle_segment)
+        super().mode_start(**kwargs)
+
+    def wiggle_segment(self, **kwargs):
+        ticks = self.machine.timers["segment_wiggle_timer"].ticks
+
+        if ticks > 58:
+            segment_number = random.randint(1, 4)
+            segment = self.machine. \
+                segment_displays["segment" + str(segment_number)]
+            segment.add_text(
+                random.randint(0,99999)
+            )
+        elif ticks > 55:
+            for i in range(1, 5):
+                self.machine. \
+                    segment_displays["segment" + str(i)]. \
+                        add_text("")
+        elif ticks > 45:
+            self.machine.segment_displays["segment4"]. \
+                add_text("1337")
+        elif ticks > 32:
+            self.machine.segment_displays["segment3"]. \
+                add_text("50")
+        elif ticks > 19:
+            self.machine.segment_displays["segment2"]. \
+                add_text("15")
+        elif ticks > 5:
+            self.machine.segment_displays["segment1"]. \
+                add_text("1986")
--- tests/test_segment_displays.py
+++ tests/test_segment_displays.py
@@ -3,22 +3,38 @@ from tests.death_save_mc_testing import DeathSaveMcTesting
 class TestSegmentDisplays(DeathSaveMcTesting):
 
     def test_segment1(self):
-         self.assertModeRunning("attract")
-         self.assertEqual(1986,
-             int(self.machine.segment_displays['segment1'].text))
+        self._attract_show_plays()
 
-         ## game begins and puts player1's score
-         ## on the segment display
-         self.hit_and_release_switch("s_start")
-         self.hit_and_release_switch("s_shooter_lane")
-         self.advance_time_and_run(1)
+        ## game begins and puts player1's score
+        ## on the segment display
+        self.hit_and_release_switch("s_start")
+        self.hit_and_release_switch("s_shooter_lane")
+        self.advance_time_and_run(1)
 
-         self.assertEqual(self.machine.game.player.score,
-             int(self.machine.segment_displays['segment1'].text))
+        self.assertEqual(self.machine.game.player.score,
+            int(self.machine.segment_displays['segment1'].text))
 
-         ## player scores 10 points by hitting the
-         ## grand advance switch scoring 10 points
-         self.hit_and_release_switch("s_grand_advance")
-         self.advance_time_and_run(1)
-         self.assertEqual(10,
-             int(self.machine.segment_displays['segment1'].text))
+        ## player scores 10 points by hitting the
+        ## grand advance switch scoring 10 points
+        self.hit_and_release_switch("s_grand_advance")
+        self.advance_time_and_run(1)
+        self.assertEqual(10,
+            int(self.machine.segment_displays['segment1'].text))
+
+    def _attract_show_plays(self):
+        self.assertModeRunning("attract")
+        self.advance_time_and_run(1)
+        self.assertEqual(1986,
+            int(self.machine.segment_displays['segment1'].text))
+
+        self.advance_time_and_run(2)
+        self.assertEqual(15,
+            int(self.machine.segment_displays['segment2'].text))
+
+        self.advance_time_and_run(4)
+        self.assertEqual(50,
+            int(self.machine.segment_displays['segment3'].text))
+
+        self.advance_time_and_run(2)
+        self.assertEqual(1337,
+            int(self.machine.segment_displays['segment4'].text))
