9729394156a6122f3a72f1aca71ba207bc7ff21c
Ryan <ryananthonyrichardson@gmail.com>
2025-04-26 12:00:03 -0700

    feat: implement backfire mode qualification

--- tests/test_backfire_mode.py
+++ tests/test_backfire_mode.py
@@ -4,10 +4,10 @@ class TestBackfireMode(DeathSaveGameTesting):
 
     # To qualify:
     #
-    # - Top saucer is lit for 15 seconds when
+    # - Top hole is lit for 15 seconds when
     #   leaving green flag mode (pit stop)
     # - During that time, first hit the spinner
-    # - Then enter the top saucer to start the mode
+    # - Then enter the top hole to start the mode
     #
     def test_qualification(self):
         self._start()
@@ -15,14 +15,46 @@ class TestBackfireMode(DeathSaveGameTesting):
         self.assertModeRunning("green_flag")
         self.assertEqual(1, self.machine.game.player.pit_count)
 
+        # qualification window not ready yet
+        self.assertEqual(False,
+            self.machine.timers["backfire_qualification_timer"].running)
+        self.assertEqual(False, self.machine. \
+            sequences["seq_backfire_qualifier"].enabled)
+
         # player returns to the pit
         self.machine.events.post("pit_required_oil")
         self.advance_time_and_run(1)
         self.assertEqual(2, self.machine.game.player.pit_count)
         self.assertModeNotRunning("green_flag")
         self.assertModeNotRunning("backfire")
+
+        # qualification timer should be running now
+        self.assertEqual(True,
+            self.machine.timers["backfire_qualification_timer"].running)
+        # sequence should be enabled
+        self.assertEqual(True, self.machine. \
+            sequences["seq_backfire_qualifier"].enabled)
+
+        # player hits the spinner
+        self.hit_and_release_switch("s_spinner")
+        self.advance_time_and_run(1)
+        # then enters the top hole
+        self.hit_and_release_switch("s_backfire_hole")
+        self.advance_time_and_run(1)
+
+        self.assertModeRunning("backfire")
+        # main timer should be running now
         self.assertEqual(True,
             self.machine.timers["backfire_timer"].running)
+
+        # ball is kicked out after 5 seconds
+        self.advance_time_and_run(5)
+
+        # timer expires after 30 more seconds and mode ends
+        self.advance_time_and_run(30)
+        self.assertEqual(False,
+            self.machine.timers["backfire_timer"].running)
+        self.assertModeNotRunning("backfire")
 
     # To complete:
     #
