eceb74a0c6ed2c26404ee6a12135620372f049c0
Ryan <ryananthonyrichardson@gmail.com>
2025-05-15 00:09:34 -0700

    feat: add grooveline state to custom code

--- modes/pit/code/pit.py
+++ modes/pit/code/pit.py
@@ -1,11 +1,15 @@
 from mpf.core.mode import Mode
 
 class Pit(Mode):
+    # Maps various states to corresponding lights
+    # For non-bool states, the value is a tuple of the
+    # light prefix and the max number in the sequence
     PROGRESS_MAP = {
         "is_grooveline_completed": "l_signal1",
         "is_luxury_completed": "l_signal2",
         "is_grand_completed": "l_signal3",
         "is_red_line_completed": "l_signal4",
+        "grooveline_counter_count": [ "l_grooveline", 10 ],
     }
 
     def mode_start(self, **kwargs):
@@ -24,9 +28,26 @@ class Pit(Mode):
     # per the PROGRESS_MAP
     def update_progress(self, **kwargs):
         player = self.machine.game.player
-        for event_name, light_name in self.PROGRESS_MAP.items():
-            if not player.is_player_var(event_name):
-                return
+        for state, value in self.PROGRESS_MAP.items():
+            print(f"Checking {state} for {value}")
+            # if not player.is_player_var(state):
+            #     return
 
-            if getattr(player, event_name) == 1:
-                self.machine.lights[light_name].on()
+            if state.startswith("is_"):
+                print(f"Syncing boolean state {state}")
+                if getattr(player, state) == 1:
+                    self.machine.lights[value].on()
+            else:
+                if not player.is_player_var(state):
+                    print(f"Skipping {state} for {value}")
+                    continue
+                print("TODO: Syncing counter")
+                # print(f"Syncing counter {state}")
+                current_state = getattr(player, state)
+                # print(f"Current counter state: {current_state}")
+                for i in range(1, value[1] + 1):
+                    light_name = f"{value[0]}_{i:0>2}"
+                    if i <= current_state:
+                        self.machine.lights[light_name].on()
+                    else:
+                        self.machine.lights[light_name].off()
