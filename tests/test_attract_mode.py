4e523fba29c3a945f6e82041853499d3e6612956
Ryan <ryananthonyrichardson@gmail.com>
2025-05-24 18:52:26 -0700

    test: fix attact test

--- tests/test_attract_mode.py
+++ tests/test_attract_mode.py
@@ -3,4 +3,4 @@ from tests.death_save_game_testing import DeathSaveGameTesting
 class TestAttractMode(DeathSaveGameTesting):
 
     def test_sound(self):
-        self._assertMusicIs("playing", "the_distance_intro")
+        self._assertMusicIs("playing", "the_distance")
