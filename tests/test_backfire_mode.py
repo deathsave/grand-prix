5bdb303d3958cb597a60dc06d301ecbe2c9cbc85
Ryan <ryananthonyrichardson@gmail.com>
2025-06-22 10:37:02 -0700

    feat: finish building out backfire mode

--- tests/test_backfire_mode.py
+++ tests/test_backfire_mode.py
@@ -37,9 +37,10 @@ class TestBackfireMode(DeathSaveGameTesting):
         self.hit_and_release_switch("s_spinner")
         self.advance_time_and_run(1)
         # then enters the top hole
-        self.hit_and_release_switch("s_backfire_hole")
-        self.advance_time_and_run(1)
+        self.hit_switch_and_run("s_backfire_hole", 1)
 
+        self.assertEqual(1,
+            self.machine.ball_devices["bd_backfire_hole"].balls)
         self.assertModeRunning("backfire")
         # main timer should be running now
         self.assertEqual(True,
@@ -47,6 +48,8 @@ class TestBackfireMode(DeathSaveGameTesting):
 
         # ball is kicked out after 5 seconds
         self.advance_time_and_run(5)
+        self.assertEqual(0,
+            self.machine.ball_devices["bd_backfire_hole"].balls)
 
         # timer expires after 30 more seconds and mode ends
         self.advance_time_and_run(30)
@@ -56,11 +59,41 @@ class TestBackfireMode(DeathSaveGameTesting):
 
     # To complete:
     #
-    # - Each consecutive pop bumper hit increases
-    #   linearly increasing the current bumpers
-    #   value by 10% up to a maximum TBD
+    # - Driver targets the pop bumpers
     # - If max is reached in time, immediately return player
     #   to green flag (fuel, lube and tires are max)
-    #
+    def test_green_flag_resume(self):
+        self._start()
+        self._start_backfire()
+
+        self.assertEqual(1,
+            self.machine.game.player.backfire_counter_count)
+        for i in range(9):
+            self.assertEqual(i + 1,
+                self.machine.game.player.backfire_counter_count)
+            self.hit_and_release_switch("s_pop1")
+            self.advance_time_and_run(1)
+        self.assertModeNotRunning("green_flag")
+
+        # one more hit and player is returned to green flag
+        self.hit_and_release_switch("s_pop1")
+        self.advance_time_and_run(1)
+        self.assertModeRunning("green_flag")
+
+        # count is always reset back to 1
+        self.assertEqual(1,
+            self.machine.game.player.backfire_counter_count)
+
     def test_scoring(self):
-        pass
+        self._start()
+        self._start_backfire()
+        self.assertModeRunning("pit")
+        self.assertModeRunning("backfire")
+        self.assertEqual(1, self.machine.game.player.multiplier)
+        for i in range(9):
+            self.assertEqual(i + 1,
+                self.machine.game.player.backfire_counter_count)
+            self._assertIncrement("score", "s_pop1", (i+1) * 1000)
+
+        # final hit with default multiplier awards 10k
+        self._assertIncrement("score", "s_pop1", 10000)
--- tests/test_multiplier.py
+++ tests/test_multiplier.py
@@ -4,7 +4,7 @@ class TestMultiplier(DeathSaveGameTesting):
 
     def test_qualification(self):
         self._start()
-        self.assertEqual(0, self.machine.game.player.multiplier)
+        self.assertEqual(1, self.machine.game.player.multiplier)
         self._start_green_flag()
         self._start_luxury()
 
@@ -16,6 +16,8 @@ class TestMultiplier(DeathSaveGameTesting):
         self.assertEqual(1, self.machine.game.player.multiplier)
 
         # after swerving 5 times, the
-        # multiplier is increated by 1
+        # multiplier is increased by 1
         self.hit_and_release_switch("s_swerve1")
         self.assertEqual(2, self.machine.game.player.multiplier)
+
+        # TODO: handle 5x multiplier
