829cc8da03328fa96aceee4a368db09e5b4f4450
Ryan <ryananthonyrichardson@gmail.com>
2025-06-24 23:00:23 -0700

    test: fix high score test

--- tests/test_high_score.py
+++ tests/test_high_score.py
@@ -3,28 +3,37 @@ from tests.death_save_game_testing import DeathSaveGameTesting
 class TestHighScore(DeathSaveGameTesting):
 
     def test_score_entry(self):
-        pass
-        # TODO: This is bugged
-        # self._start()
-        # for ball in range(1, 4):
-        #     self._expire_ball_save()
-        #     self.assertEqual(ball, self.machine.game.player.ball)
+        self._start()
 
-        #     # ball drains and next ball begins
-        #     self._drain_one_ball()
-        #     self.advance_time_and_run(4)
-        #     self.hit_and_release_switch("s_activate_playfield")
+        # blow through the first two balls
+        for ball in range(1, 3):
+            self.hit_and_release_switch("s_shooter_lane")
+            self.hit_and_release_switch("s_activate_playfield")
+            self._expire_ball_save()
+            self.assertEqual(ball, self.machine.game.player.ball)
 
-        # self._expire_ball_save()
-        # # skip to ball 3
-        # self.assertEqual(3, self.machine.game.player.ball)
-        # for i in range(30):
-        #     self.hit_and_release_switch("s_pit_lube")
+            # ball drains and next ball begins
+            self._drain_one_ball()
+            self.advance_time_and_run(10)
 
-        # self.assertModeRunning("high_score")
-        # current_widgets = self.get_text_widgets()
-        # self.assertIn("Driver 1",
-        #     [x.text for x in current_widgets])
+        # now we're on ball 3
+        self.assertEqual(3, self.machine.game.player.ball)
+        self.hit_and_release_switch("s_shooter_lane")
+        self._expire_ball_save()
+        self.advance_time_and_run(10)
+        self.assertEqual(2,
+            self.machine.ball_devices["bd_trough"].balls)
+
+        # score a bunch of points
+        for i in range(30):
+            self.hit_and_release_switch("s_pit_lube")
+        self._drain_one_ball()
+        self.advance_time_and_run(10)
+
+        self.assertModeRunning("high_score")
+        current_widgets = self.get_text_widgets()
+        self.assertIn("Driver 1",
+            [x.text for x in current_widgets])
 
     def get_text_widgets(self):
         current_widgets = []
