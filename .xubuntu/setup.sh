ef4182a15039dc514f61dcbdf66232527499d639
Ryan Richardson <ryananthonyrichardson@gmail.com>
2025-12-02 20:34:50 -0800

    fix: adjust setup script for new username

--- .xubuntu/setup.sh
+++ .xubuntu/setup.sh
@@ -4,10 +4,16 @@ sudo groupadd -r autologin
 sudo gpasswd -a $USER autologin
 
 # To autologin to the Xubuntu Desktop X Server
+if [ -f "/etc/lightdm/lightdm.conf" ]; then
+    sudo rm "/etc/lightdm/lightdm.conf"
+fi
 sudo ln -s $HOME/grand-prix/.xubuntu/lightdm.conf \
   /etc/lightdm/lightdm.conf
 
 # To launch MPF after starting the X Server
+if [ -f "$HOME/.config/autostart/mpf.desktop" ]; then
+    rm "$HOME/.config/autostart/mpf.desktop"
+fi
 mkdir -p $HOME/.config/autostart
 ln -s $HOME/grand-prix/.xubuntu/mpf.desktop \
   $HOME/.config/autostart/mpf.desktop
@@ -24,4 +30,5 @@ sudo sed -i -e 's/XKBOPTIONS=\"\"/XKBOPTIONS=\"caps:backspace\"/g' /etc/default/
 sudo dpkg-reconfigure keyboard-configuration
 
 # Alias serial devices
+[ -f "/etc/udev/rules.d/opp.rules" ] && sudo rm "/etc/udev/rules.d/opp.rules"
 sudo ln -s $HOME/grand-prix/.xubuntu/opp.rules /etc/udev/rules.d/opp.rules
